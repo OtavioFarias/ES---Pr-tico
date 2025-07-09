099de5c6708659b28b7db37916b6af4bbac96c8f
Otavio_Farias <otaviogarcia.aluno@unipampa.edu.br>
2025-07-09 18:01:28 -0300

    .

--- implementacao/funcional/Tools/ArqDiscente.java
+++ implementacao/funcional/Tools/ArqDiscente.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.List;
 import classes.atributos.*;
 
